97da63b8c84713f857f7fde08cd16cc0ca23ff19
CrosszPai <43898498+CrosszPai@users.noreply.github.com>
2020-01-25 19:03:49 +0700

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <jsonwrapper.hpp>
 
-using string = char*;
-
 void setup()
 {
   json j;
@@ -12,35 +10,38 @@ void setup()
   // size should be calculated by ArduinoJson Assistant https://arduinojson.org/v6/assistant/
   DynamicJsonDocument maindoc(size);
   int i[] = {1, 2, 3, 4};
-  int b[2][2] = {{2, 123}, {233, 18}};
+  int b[2][3] = {{2, 12, 123}, {233, 18, 123}};
   int dictionary[3][2] = {{23, 68}, {30, 70}, {22, 21}};
-  auto dict_key(unique_ptr<string>( new char *[10] { "temp", "hum" })); 
+  auto dict_key(unique_ptr<char *>(new char *[10] { "temp", "hum" }));
   maindoc["text"] = "Hello World";
   maindoc["number"] = 23.42f;
   maindoc["boolean"] = false;
   j.array_add(&maindoc, "single_arr", i, sizeof(i) / sizeof(i[0]));
   maindoc["single_arr"].add(123);
-  auto tchar(unique_ptr<string>(new char *[10] { "value1", "value2", "value3", "value4" }));
+  auto tchar(unique_ptr<char *>(new char *[10] { "value1", "value2", "value3", "value4" }));
   j.object_add(&maindoc, i, "general_obj", 4, tchar.raw_ptr());
-  bool logic[] = {true, false, false};   
-  auto a(unique_ptr<string>(new char *[10] { "a", "b", "c" }));
+  bool logic[] = {true, false, false};
+  auto a(unique_ptr<char *>(new char *[10] { "a", "b", "c" }));
   j.object_add(&maindoc, logic, "general_obj", 3, a.raw_ptr());
-  j.nested_array(&maindoc["test"]["two_dim_arr"], b, 2, 2);
-  j.nested_object(&maindoc["test"]["array_of_object"], dictionary, 3, 2, dict_key.raw_ptr());
+  auto w = maindoc["test"]["arr_arr"];
+  j.nested_array(&w, b, 2, 3);
+  auto aw = maindoc["test"]["arr_obj"];
+  j.nested_object(&aw, dictionary, 3, 2, dict_key.raw_ptr());
   maindoc["obj"]["nest"]["obj"]["in"] = 2;
+  maindoc["obj"]["nest"]["obj"]["in"] = false;
   serializeJsonPretty(maindoc, Serial);
   /*
   {
   "text": "Hello World",
   "number": 23.42,
   "boolean": false,
-  "single_arr": [  
+  "single_arr": [
     1,
-    2, 
-    3, 
-    4, 
+    2,
+    3,
+    4,
     123
-  ],
+  ],   
   "general_obj": {
     "value1": 1,
     "value2": 2,
@@ -51,17 +52,19 @@ void setup()
     "c": false
   },
   "test": {
-    "arr_arr_nested": [
+    "arr_arr": [
       [
         2,
+        12,
         123
       ],
       [
         233,
-        18
+        18,
+        123
       ]
     ],
-    "object_arr_nested": [
+    "arr_obj": [
       {
         "temp": 23,
         "hum": 68
@@ -79,12 +82,12 @@ void setup()
   "obj": {
     "nest": {
       "obj": {
-        "in": 2
+        "in": false
       }
     }
   }
 }
-  */
+*/
 }
 
 void loop()
